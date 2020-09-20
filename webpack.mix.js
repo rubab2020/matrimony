b145fbfdbb1b8b1732a372ca5b4ad14376b7cba4
Risul Islam <risul321@gmail.com>
2020-09-21 03:31:48 +0600

    Admin assets and authentication

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,69 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [require("postcss-import"), require("tailwindcss")]
+);
+
+mix.styles(
+    [
+        "node_modules/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css",
+        "node_modules/@fortawesome/fontawesome-free/css/all.css",
+        "node_modules/gentelella/vendors/iCheck/skins/flat/green.css",
+        "node_modules/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css",
+        "node_modules/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css",
+        "node_modules/gentelella/vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css",
+        "node_modules/gentelella/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css",
+        "node_modules/gentelella/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css",
+        "node_modules/gentelella/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css",
+        "node_modules/gentelella/vendors/pnotify/dist/pnotify.css",
+        "node_modules/gentelella/vendors/pnotify/dist/pnotify.buttons.css",
+        "node_modules/gentelella/vendors/select2/dist/css/select2.min.css",
+        "node_modules/gentelella/build/css/custom.min.css",
+    ],
+    "public/assets/admin/css/admin.css"
+).version();
+
+mix.scripts(
+    [
+        "node_modules/gentelella/vendors/jquery/dist/jquery.min.js",
+        "node_modules/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js",
+        "node_modules/@fortawesome/fontawesome-free/js/all.js",
+        "node_modules/gentelella/vendors/fastclick/lib/fastclick.js",
+        "node_modules/gentelella/vendors/nprogress/nprogress.js",
+        "node_modules/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js",
+        "node_modules/gentelella/vendors/iCheck/icheck.min.js",
+        "node_modules/gentelella/vendors/moment/min/moment.min.js",
+        "node_modules/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.js",
+        "node_modules/gentelella/vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js",
+        "node_modules/gentelella/vendors/datatables.net/js/jquery.dataTables.min.js",
+        "node_modules/gentelella/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js",
+        "node_modules/gentelella/vendors/datatables.net-responsive/js/dataTables.responsive.min.js",
+        "node_modules/gentelella/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js",
+        "node_modules/gentelella/vendors/datatables.net-scroller/js/dataTables.scroller.min.js",
+        "node_modules/gentelella/vendors/pnotify/dist/pnotify.js",
+        "node_modules/gentelella/vendors/pnotify/dist/pnotify.buttons.js",
+        "node_modules/gentelella/vendors/select2/dist/js/select2.full.min.js",
+        "node_modules/gentelella/build/js/custom.js",
+    ],
+    "public/assets/admin/js/admin.js"
+)
+    .version()
+    .sourceMaps();
+
+mix.copy(
+    ["node_modules/gentelella/vendors/bootstrap/dist/fonts"],
+    "public/assets/admin/fonts"
+);
+
+mix.copy(
+    ["node_modules/@fortawesome/fontawesome-free/webfonts"],
+    "public/assets/admin/webfonts"
+);
+
+mix.copy(
+    ["node_modules/gentelella/production/images"],
+    "public/assets/admin/images"
+);
