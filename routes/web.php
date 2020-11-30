773b9f7e32fa2d57fdbf96acb988492dfda2204d
Risul Islam <risul321@gmail.com>
2020-12-01 00:48:23 +0600

    Updated web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ShareProfileController;
-use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +32,7 @@ Route::get('/search', function () {
 Route::get('/profile', function () {
     return view('website.profile.profile');
 });
-Route::get('/shareprofile/share','App\Http\Controllers\ShareProfileController@share')->name('shareprofile.share');
+Route::get('/shareprofile/share', 'ShareProfileController@share')->name('shareprofile.share');
 Route::get('profile/create', 'ProfileController@create');
 Route::post('profile/save', 'ProfileController@store');
 
@@ -60,13 +58,12 @@ Route::get('/verify/phone', 'OTPController@sendOTP');
 Route::post('/otp/send', 'OTPController@sendOTP')->name('otp.send');
 Route::post('/otp/resend', 'OTPController@resendOTP')->name('otp.resend');
 Route::post('/otp/verify', 'OTPController@verifyOTP')->name('otp.verify');
-Route::get('/message','MessageController@index')->name('message');
+Route::get('/message', 'MessageController@index')->name('message');
 Route::get('/message/{id}', 'MessageController@getMessage')->name('message/id');
 Route::post('send_message', 'MessageController@sendMessage');
 
-Route::group(['prefix' => 'customer', 'middleware' => ['auth:customer']], function() {
-	Route::get('dashboard', function() {
-		return view('website.dashboard');
-	})->name('customer.dashboard');
+Route::group(['prefix' => 'customer', 'middleware' => ['auth:customer']], function () {
+    Route::get('dashboard', function () {
+        return view('website.dashboard');
+    })->name('customer.dashboard');
 });
-
