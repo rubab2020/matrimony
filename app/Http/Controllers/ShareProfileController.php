8c7b8aa9264895358a7a822168d6ace4a77e4c00
Risul Islam <risul321@gmail.com>
2021-02-22 23:00:41 +0600

    Updated share profile

--- app/Http/Controllers/ShareProfileController.php
+++ app/Http/Controllers/ShareProfileController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -8,13 +9,13 @@ use App\Jobs\SendSMS;
 
 class ShareProfileController extends Controller
 {
-    public function share(Request $request){
+    public function share(Request $request)
+    {
         $phoneNumber   = $request->input('phoneNumber');
-        $customer_id= 1;
-        $url = "http://".$_SERVER['HTTP_HOST']."/profile/".$customer_id;
-        $ValidUrl = str_replace("&","&amp;",$url);
-        $message = "You have a profile recommendation.Please click the link to watch it : ".$ValidUrl;
-        dd($message);
-        //SendSMS::dispatch($phoneNumber, $message);
+        $customer_id = 1;
+        $url = url("/profile/" . $customer_id);
+        $message = "You have a profile recommendation.Please click the link to watch it : " . $url;
+
+        SendSMS::dispatch($phoneNumber, $message);
     }
 }
